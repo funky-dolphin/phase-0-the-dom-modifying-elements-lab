776743539d244293a32d97743ca8f6e083955879
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-02-06 20:52:26 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const findMain = document.querySelector("#main").remove()
+const newHeader = document.createElement("h1")
+newHeader.id= "victory"
+newHeader.textContent="Nick Brereton is the champion"
+document.append(newHeader)
